0375c07fcc9c320b8410fc6219a78c735587fb77
sarbada1 <sanjelsarbada12@gmail.com>
2023-01-21 22:00:56 +0545

    Grocery Website

--- COMPONENT2/JS/script.js
+++ COMPONENT2/JS/script.js
@@ -5,14 +5,53 @@ let navbar= document.querySelector('.navbar');
 
 document.querySelector('#search-btn').onclick= () => {
     searchform.classList.toggle('active');
+    cart.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
 }
 document.querySelector('#cart-btn').onclick=()=>{
     cart.classList.toggle('active');
+    searchform.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
 }
 document.querySelector('#user-btn').onclick=()=>{
     loginform.classList.toggle('active');
+    searchform.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
 }
 document.querySelector('#menu-btn').onclick=()=>{
     navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    cart.classList.remove('active');
+    loginform.classList.remove('active');
 }
+window.onscroll = () => {
+    searchform.classList.remove('active');
+    cart.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay:7500,
+        disabbleOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,       
+      },
+      768: {
+        slidesPerView: 2,      
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
 
