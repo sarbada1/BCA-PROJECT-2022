9ea296d80b04bca98858e319116d34f357664335
sarbada1 <sanjelsarbada12@gmail.com>
2023-01-22 22:11:22 +0545

    Grocery project

--- COMPONENT2/JS/script.js
+++ COMPONENT2/JS/script.js
@@ -34,24 +34,5 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
 }
-var swiper = new Swiper(".product-slider", {
-    loop:true,
-    spaceBetween: 20,
-    autoplay: {
-        delay:7500,
-        disabbleOnInteraction:false,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,       
-      },
-      768: {
-        slidesPerView: 2,      
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-    },
-  });
 
 
